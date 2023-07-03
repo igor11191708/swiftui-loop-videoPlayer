e99645c2c8d257470d5c5616942b04920e817b67
Igor <igorshelopaev@gmail.com>
2023-07-03 14:21:44 +0500

    Update swiftui_loop_videoplayerTests.swift

--- Tests/swiftui-loop-videoplayerTests/swiftui_loop_videoplayerTests.swift
+++ Tests/swiftui-loop-videoplayerTests/swiftui_loop_videoplayerTests.swift
@@ -6,6 +6,6 @@ final class swiftui_loop_videoplayerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(swiftui_loop_videoplayer().text, "Hello, World!")
+        // XCTAssertEqual(swiftui_loop_videoplayer().text, "Hello, World!")
     }
 }
