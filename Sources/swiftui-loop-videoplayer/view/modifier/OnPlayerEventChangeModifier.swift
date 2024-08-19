51ace544e1e9f480f1c0c46529cba9821c7b9d41
Igor <igorshelopaev@gmail.com>
2024-08-19 19:15:30 +0200

    update

--- Sources/swiftui-loop-videoplayer/view/modifier/OnPlayerEventChangeModifier.swift
+++ Sources/swiftui-loop-videoplayer/view/modifier/OnPlayerEventChangeModifier.swift
@@ -7,25 +7,32 @@
 
 import SwiftUI
 
+/// Defines a custom `PreferenceKey` for handling player events within SwiftUI views.
 internal struct PlayerEventPreferenceKey: PreferenceKey {
+    /// The default value of player events, initialized as an empty array.
     public static var defaultValue: [PlayerEvent] = []
+    
+    /// Aggregates values from the view hierarchy when child views provide values.
     public static func reduce(value: inout [PlayerEvent], nextValue: () -> [PlayerEvent]) {
         value = nextValue()
     }
 }
 
+/// A view modifier that monitors changes to player events and triggers a closure.
 internal struct OnPlayerEventChangeModifier: ViewModifier {
+    /// The closure to execute when player events change.
     var onPlayerEventChange: ([PlayerEvent]) -> Void
 
+    /// Attaches a preference change listener to the content and executes a closure when player events change.
     func body(content: Content) -> some View {
         content
-            .onPreferenceChange(PlayerEventPreferenceKey.self) { event in
-                onPlayerEventChange(event)
-            }
+            .onPreferenceChange(PlayerEventPreferenceKey.self, perform: onPlayerEventChange)
     }
 }
 
+/// Extends `View` to include a custom modifier for handling player event changes.
 public extension View {
+    /// Applies the `OnPlayerEventChangeModifier` to the view to handle player event changes.
     func onPlayerEventChange(perform action: @escaping ([PlayerEvent]) -> Void) -> some View {
         self.modifier(OnPlayerEventChangeModifier(onPlayerEventChange: action))
     }
--- Sources/swiftui-loop-videoplayer/view/modifier/OnTimeChangeModifier.swift
+++ Sources/swiftui-loop-videoplayer/view/modifier/OnTimeChangeModifier.swift
@@ -7,25 +7,32 @@
 
 import SwiftUI
 
+/// Defines a custom `PreferenceKey` for storing and updating the current playback time.
 internal struct CurrentTimePreferenceKey: PreferenceKey {
+    /// Sets the default playback time to 0.0 seconds.
     public static var defaultValue: Double = 0.0
+    
+    /// Aggregates the most recent playback time from child views.
     public static func reduce(value: inout Double, nextValue: () -> Double) {
         value = nextValue()
     }
 }
 
+/// A view modifier that listens for changes in playback time and triggers a response.
 internal struct OnTimeChangeModifier: ViewModifier {
+    /// The closure to execute when there is a change in playback time.
     var onTimeChange: (Double) -> Void
 
+    /// Attaches a preference change listener to the content that triggers `onTimeChange` when the playback time updates.
     func body(content: Content) -> some View {
         content
-            .onPreferenceChange(CurrentTimePreferenceKey.self) { time in
-                onTimeChange(time)
-            }
+            .onPreferenceChange(CurrentTimePreferenceKey.self, perform: onTimeChange)
     }
 }
 
-public extension View{
+/// Extends `View` to include functionality for responding to changes in playback time.
+public extension View {
+    /// Applies the `OnTimeChangeModifier` to the view to manage updates in playback time.
     func onPlayerTimeChange(perform action: @escaping (Double) -> Void) -> some View {
         self.modifier(OnTimeChangeModifier(onTimeChange: action))
     }
