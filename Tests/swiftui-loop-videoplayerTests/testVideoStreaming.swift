534310b34fc0363d5299b0a726551e7f560c10da
Igor <igorshelopaev@gmail.com>
2024-08-26 17:31:45 +0200

    Update testVideoStreaming.swift

--- Tests/swiftui-loop-videoplayerTests/testVideoStreaming.swift
+++ Tests/swiftui-loop-videoplayerTests/testVideoStreaming.swift
@@ -6,21 +6,23 @@
 //
 
 import XCTest
-import SwiftUI
 @testable import swiftui_loop_videoplayer
 
 final class testVideoStreaming: XCTestCase {
 
+    
     func testLocalVideoStreaming() {
-
-        guard let filePath = Bundle.module.path(forResource: "swipe", ofType: "mp4") else {
-            XCTFail("Missing file: swipe.mp4")
-            return
-        }
         
-        let _ = ExtVideoPlayer(fileName: filePath)
-        // Use the filePath as needed for your tests
-        print("Video file path: \(filePath)")
+        let expectation = XCTestExpectation(description: "Playing event should be fired")
+           
+
+           // Setup player with video
+           guard let filePath = Bundle.module.path(forResource: "swipe", ofType: "mp4") else {
+               XCTFail("Missing file: swipe.mp4")
+               return
+           }
+        
+        let playerView = ExtVideoPlayer(fileName: filePath)
 
     }
 }
